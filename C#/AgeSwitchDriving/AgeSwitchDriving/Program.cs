2716b28994827c7d262a93491687d1fb09318ba6
Jacek <108802573+yatsecs@users.noreply.github.com>
2023-01-31 14:18:07 +0000

    Add files via upload

---
+++ C#/AgeSwitchDriving/AgeSwitchDriving/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        int age;
+        Console.WriteLine("What you age");
+        age = Convert.ToInt32(Console.ReadLine());
+
+        switch (age)
+        {
+            case < 0:
+                Console.WriteLine("Invalid");
+                break;
+
+            case < 16:
+                Console.WriteLine("No drive");
+                break;
+
+            case < 120:
+                Console.WriteLine("Yes driving");
+                break;
+
+            case > 120:
+                Console.WriteLine("You are not that age");
+                break;
+
+            default:
+                Console.WriteLine("Invalid");
+                break;
+
+        }
+    }
+}
