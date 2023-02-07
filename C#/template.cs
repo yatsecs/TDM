f10818ad29393acfdfbb26a570f48830026221e6
Jacek <108802573+yatsecs@users.noreply.github.com>
2023-02-07 09:45:20 +0000

    Create template.cs

---
+++ C#/template.cs
@@ -0,0 +1,14 @@
+using System;
+namespace ChangeName
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("test");
+        }
+
+
+
+    }
+}
